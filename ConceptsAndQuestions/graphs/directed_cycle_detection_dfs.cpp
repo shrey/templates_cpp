89f99e286e07e25dc656824bcbcd2dc728b032a9
Shrey Dubey <wshrey09@gmail.com>
2020-07-31 19:58:15 +0530

    hdsof

--- ConceptsAndQuestions/graphs/directed_cycle_detection_dfs.cpp
+++ ConceptsAndQuestions/graphs/directed_cycle_detection_dfs.cpp
@@ -16,6 +16,7 @@
 #define S second
 #define umap unordered_map
 #define fo(x,y) for(int i = x; i<y; i++)
+#define fo(n) for(int i = 0; i<n; i++)
 #define fnd(stl, data) find(stl.begin(), stl.end(), data)
 
 using namespace std;
@@ -26,7 +27,7 @@ class Graph{
     umap <int,list<int> > adjlist;
     umap<int,bool> visited;
     list<int> sorted_nums;
-    list<int> node_stack;
+    umap<int,bool> node_stack;
     bool is_cyclic = false;
     int n;
     public:
@@ -39,14 +40,41 @@ class Graph{
         //do dfs, if repeated el occurs and check if it already it exists in the stack
         void dfs(int src){
             if(visited[src]){
-                // if(find(node_stack.begin(), node_stack.end(), src)){
-                //     is_cyclic = true;
-                // };
+                if(node_stack[src]){
+                    is_cyclic = true;
+                }
+                return;
             }
-            node_stack.push_front(src);
+            node_stack[src] = true;
             for(auto it: adjlist[src]){
                 dfs(it);
+                if(is_cyclic){
+                    return;
+                }
             }
-            node_stack.pop_front();
+            node_stack[src] = false;
+        }
+        void ans(){
+            if(is_cyclic){
+                cout<<"CYCLIC"<<endl;
+            }
+            else{
+                cout<<"no"<<endl;
+            }
         }
 };
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    Graph g(n);
+    int x,y;
+    fo(m){
+        cin>>x>>y;
+        g.addEdge(x,y);
+
+
+    }
+    g.dfs(0);
+    g.ans();
+}

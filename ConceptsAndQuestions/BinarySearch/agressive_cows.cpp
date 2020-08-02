15f79c0aec39ceef1bb24298f0e2f298f2897f81
Shrey Dubey <wshrey09@gmail.com>
2020-08-03 03:11:52 +0530

    message

--- ConceptsAndQuestions/BinarySearch/agressive_cows.cpp
+++ ConceptsAndQuestions/BinarySearch/agressive_cows.cpp
@@ -32,40 +32,43 @@ using namespace std;
 typedef long long ll;
 int modulo = 1e9 + 7;
 
-//here, the monotonic search space is 0 to n 🚀
 
-float square_root(int n){
-    int s = 0;
-    float ans = 0;
-    int e = n;
-    while(s<=e){
-        int mid = (s+e)/2;
-        if(mid*mid == n){
-            return mid;
-        }
-        if(mid*mid<n){
-            if(mid>ans) ans = mid;
-            s = mid+1;
-        }
-        if(mid*mid>n){
-            e = mid-1;
+bool canPlaceCows(int stalls[], int n, int cows, int min_sep){
+    int last_cow = stalls[0];
+    int cnt = 1;
+    for(int i = 1; i<n; i++){
+        if((stalls[i]-last_cow)>=min_sep){
+            last_cow = stalls[i];
+            cnt++;
+            if(cnt == cows){
+                return true;
+            }
         }
     }
-
-    float inc = 0.1;
-    for(int i = 1; i<=5; i++){
-        while(ans*ans<=n){
-            ans = ans + inc;
-        }
-        ans = ans - inc;
-        inc/=10;
-    }
-    return ans;
+    return false;
 }
 
-
 int main(){
-    int n;
-    cin>>n;
-    cout<<square_root(n)<<endl;
+    int stalls[] = {1,2,4,8,9};
+    int n = sizeof(stalls)/sizeof(int);
+    int cows = 3;
+    int s = 0;
+    int e = stalls[n-1]-stalls[0];
+    int ans = 0;
+    while(s<=e){
+        int mid = (s+e)/2;
+
+        bool cows_rakh_paye = canPlaceCows(stalls,n,cows,mid);
+
+        if(cows_rakh_paye){
+            // cout<<mid<<endl;
+            ans = mid;
+            s = mid+1;
+        }
+        else{
+            e = mid-1;
+        }
+
+    }
+    cout<<ans<<endl;
 }

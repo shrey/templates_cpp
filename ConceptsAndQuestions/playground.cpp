0d0f9c482d8de2688d030e8f23b17c15a87d3937
Shrey Dubey <wshrey09@gmail.com>
2020-08-23 17:35:09 +0530

    p

---
+++ ConceptsAndQuestions/playground.cpp
@@ -0,0 +1,41 @@
+
+
+//Shrey Dubey
+
+//Contact Me at wshrey09@gmail.com
+
+
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<unordered_map>
+#include<vector>
+#include<set>
+#include<list>
+#include<iomanip>
+#include<queue>
+#include<stack>
+#include <math.h>
+
+
+
+#define prDouble(x) cout<<fixed<<setprecision(10)<<x //to print decimal numbers
+#define pb push_back
+#define F first
+#define S second
+#define umap unordered_map
+#define mp make_pair
+
+#define fo(n) for(int i = 0; i<n; i++)
+#define fnd(stl, data) find(stl.begin(), stl.end(), data)
+
+using namespace std;
+typedef long long ll;
+int modulo = 1e9 + 7;
+
+int main(){
+    string s = "shrey";
+    cout<<s.substr(3)<<endl;
+}
